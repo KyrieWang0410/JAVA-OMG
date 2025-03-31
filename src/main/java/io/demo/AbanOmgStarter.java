a5fc0f2b0259b11736ab202136f4e84019a96e1b
kyrie.wang <kyrie.wang@qq.com>
2025-03-31 15:23:08 +0800

    项目初始化

---
+++ src/main/java/io/demo/AbanOmgStarter.java
@@ -0,0 +1,11 @@
+package io.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbanOmgStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(AbanOmgStarter.class, args);
+    }
+}
