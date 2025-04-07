16741cf5517d55a861f12199866ac9829566bbdd
kyrie.wang <kyrie.wang@qq.com>
2025-04-08 00:47:49 +0800

    接口方法支持get post方式

--- src/main/java/io/demo/controller/BaseController.java
+++ src/main/java/io/demo/controller/BaseController.java
@@ -1,15 +1,16 @@
 package io.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/base")
 public class BaseController {
 
-    @GetMapping("/status")
+    @RequestMapping(value = "/status", method = {RequestMethod.GET, RequestMethod.POST})
     public String index() {
         return "Hello,Spring Boot!";
     }
+
 }
