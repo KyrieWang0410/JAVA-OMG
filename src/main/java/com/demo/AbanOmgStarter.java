4787e0276d65aa016a556e0e464619142da30152
kyrie.wang <kyrie.wang@qq.com>
2025-04-23 14:32:24 +0800

    添加Mybatis-Plus依赖，新增H2数据库

--- src/main/java/com/demo/AbanOmgStarter.java
+++ src/main/java/com/demo/AbanOmgStarter.java
@@ -1,9 +1,11 @@
-package io.demo;
+package com.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.demo.mapper")
 public class AbanOmgStarter {
     public static void main(String[] args) {
         SpringApplication.run(AbanOmgStarter.class, args);
---
+++ src/test/java/com/demo/ServiceTest.java
@@ -0,0 +1,34 @@
+package com.demo;
+
+import com.demo.domain.User;
+import com.demo.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+class ServiceTest {
+
+    private final UserMapper userMapper;
+
+    @Test
+    void testSelect() {
+        log.warn(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        assertThat(userList)
+                .hasSize(5)
+                .isNotEmpty();
+        userList.forEach(user -> log.info(user.toString()));
+    }
+}
