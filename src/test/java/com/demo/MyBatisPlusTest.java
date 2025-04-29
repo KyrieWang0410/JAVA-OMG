62159a2d8d38a17161736ee4b8e138b4cf6f756b
kyrie.wang <kyrie.wang@qq.com>
2025-04-29 17:50:06 +0800

    修改Mybatis-Plus集成MySQL

---
+++ src/test/java/com/demo/MyBatisPlusTest.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+import com.demo.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+class MyBatisPlusTest {
+
+    private final UserMapper userMapper;
+
+    @Test
+    void queryTest() {
+        log.info("MySQL版本查询为{}", userMapper.queryVersion());
+    }
+}
